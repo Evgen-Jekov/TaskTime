79d9b2ba7f37b707f3cfd9becf042f4eb392f40a
Evgen-Jekov <zekovevgen992@gmail.com>
2025-04-23 01:10:26 +0000

    the foundation for user is laid

---
+++ api/app/schemes/user_schemas.py
@@ -0,0 +1,13 @@
+from app.core.extensions import ma
+from app.model.user_model import UserModel
+from marshmallow import fields, post_load
+
+class UserSchemes(ma.Schema):
+    id = fields.Integer(required=True, dump_only=True)
+    username = fields.String(required=True)
+    email = fields.String(required=True)
+    password = fields.String(required=True)
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return UserModel(**data)
