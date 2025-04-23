28d8de5644914078566d0a5e0f3c11f8af46dc4e
Evgen-Jekov <zekovevgen992@gmail.com>
2025-04-23 01:17:55 +0000

    create delete User

---
+++ api/app/repository/user.py
@@ -0,0 +1,16 @@
+from app.repository.database_abc import DeleteDB, UpdateDB, SearchDB
+from app.model.user_model import UserModel
+from app.repository.auxiliary import check_data_id
+from app.core.extensions import db
+from sqlalchemy.exc import SQLAlchemyError
+
+class DeleteUser(DeleteDB):
+    def delete_db(self, id):
+        try:
+            user = check_data_id(model=UserModel, id=id)
+
+            db.session.delete(user)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            raise SQLAlchemyError(str(e))
